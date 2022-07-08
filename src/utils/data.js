c0ef433b67af96dff498ea5e3d60b083e9eaa546
imasshaWe <imasha98.we@gmail.com>
2022-07-08 13:23:34 +0530

    update design

--- src/utils/data.js
+++ src/utils/data.js
@@ -21,12 +21,19 @@ export const portfolio = [
         title: 'Learning Management System',
         type: 'web',
         project: 'open',
-        device: 'device-surface-pro',
+        device: 'device-imac-pro',
         description: "First Srilanka's online stereo radio channel which can hear 24 hours from all over the world streaming from south Korea. Every Sri Lankan can join and enjoy music, listen to many programs.",
         cover: 'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657207746/portfolio/Screenshot_7_7_2022_8_22_10_PM_hdbumt.png',
         images: [
-            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657207746/portfolio/Screenshot_7_7_2022_8_22_10_PM_hdbumt.png',
-            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657207759/portfolio/React_App_-_Google_Chrome_7_7_2022_8_32_57_PM_opy24i.png'
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258245/portfolio/lms/Screenshot_64_ntkuok.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258282/portfolio/lms/Screenshot_60_qy97yo.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258278/portfolio/lms/Screenshot_65_w7aedg.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258281/portfolio/lms/Screenshot_68_xw5pxh.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258323/portfolio/lms/Screenshot_71_pjgm0w.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258323/portfolio/lms/Screenshot_71_pjgm0w.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258232/portfolio/lms/Screenshot_69_ptgqfw.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258232/portfolio/lms/Screenshot_70_umkrhd.png',
+            'https://res.cloudinary.com/dxrhrbot0/image/upload/v1657258318/portfolio/lms/Screenshot_61_odckwh.png',
         ],
         links: [
             {type: 'github', 'url': 'https://github.com/imashaWe/LMS'},
